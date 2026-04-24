790bedaa81fac9713cca733929cff5d17eae5a44
Nguyễn Văn Luyến <nguyenluyen@nsg.edu.vn>
2026-04-24 15:53:12 +0700

    feat: thêm thanh tiến trình (nextjs-toploader) khi chuyển trang để tăng trải nghiệm UX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from 'nextjs-toploader';
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700', '900'],
@@ -23,7 +24,20 @@ export default function RootLayout({
       lang="vi"
       className={`${roboto.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans">{children}</body>
+      <body className="min-h-full flex flex-col font-sans">
+        <NextTopLoader 
+          color="#3b82f6" 
+          initialPosition={0.08} 
+          crawlSpeed={200} 
+          height={3} 
+          crawl={true} 
+          showSpinner={true} 
+          easing="ease" 
+          speed={200} 
+          shadow="0 0 10px #3b82f6,0 0 5px #3b82f6" 
+        />
+        {children}
+      </body>
     </html>
   );
 }
