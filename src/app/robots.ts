2b6ae4fe596ae9b5010e55cac424b408aabc00b9
Nguyễn Văn Luyến <nguyenluyen@nsg.edu.vn>
2026-06-09 10:08:43 +0700

    feat: Add sitemap.xml and robots.txt for SEO optimization

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://cokhi.namsaigon.edu.vn'
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/dashboard/'],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
